6fe53195950e4851b8f6f8ca2e2bf43a4809fb11
Esandi99 <156070393+Esandi99@users.noreply.github.com>
2026-07-08 15:34:18 +0530

    Enhance ddl_bronze.sql with comments
    Added comments to describe the purpose of the script and the actions taken.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
 
@@ -70,6 +81,5 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 
 
 
-EXEC bronze.load_bronze
 
 
